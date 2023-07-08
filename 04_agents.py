3bff2bc74c6fda1668c9dd03a7b53681877dc7d6
mendhak <mendhak@users.noreply.github.com>
2023-07-08 16:16:42 +0100

    Using openweathermap to search

--- 04_agents.py
+++ 04_agents.py
@@ -22,17 +22,6 @@ llm = AzureOpenAI(
     temperature=0.1
 )
 
-# tools = load_tools(["serpapi", "llm-math"], llm=llm)
-
-# agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, handle_parsing_errors=True)
-
-# output = agent.run("What was the high temperature in SF yesterday in Fahrenheit? What is that number raised to the .023 power?")
-
-# print(output)
-
-
-# Using an agent with chat model (it still requires an llm)
-
 from langchain.chat_models import AzureChatOpenAI
 chat = AzureChatOpenAI(
     openai_api_base=OPENAI_API_BASE,
@@ -43,9 +32,9 @@ chat = AzureChatOpenAI(
     temperature=0
 )
 
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
+tools = load_tools(["openweathermap-api", "llm-math"], llm=llm)
 
 agent = initialize_agent(tools, chat, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, handle_parsing_errors=True)
 
 # Now let's test it out!
-agent.run("What was the high temperature in London yesterday in Celsius? What is that number multiplied by five?")
+agent.run("What is the temperature in Helsinki right now? What is that number doubled?")
